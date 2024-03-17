0b57b26cb3bd0dbd2cb7bc6dc966933d7f4ee08e
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-17 19:37:50 -0400

    Added separate file for nix global parameters

---
+++ modules/editors/neovim/lua/config/global_parameters_nix.lua
@@ -0,0 +1,43 @@
+-- Setting 'packpath' equal to 'runtimepath'
+vim.opt.packpath = vim.opt.runtimepath:get()
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.lua",
+  callback = function()
+    vim.opt_local.syntax = "lua"
+  end,
+})
+
+-- Set number and relative number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable syntax highlighting
+vim.cmd('syntax on')
+
+-- Show command in the bottom bar
+vim.opt.showcmd = true
+
+-- Set tab stops
+vim.opt.tabstop = 4
+
+-- Uncomment the next line if you want to enable spell checking
+-- vim.opt.spell = true
+
+-- Set auto indentation
+vim.opt.autoindent = true
+
+-- Uncomment the next line if you want to use the system clipboard
+-- vim.opt.clipboard = 'unnamedplus'
+
+-- Set the background to dark
+vim.opt.background = 'dark'
+
+-- Set file encoding
+vim.opt.encoding = 'utf-8'
+
+-- Set fold method
+vim.opt.foldmethod = "syntax"
+
+-- Disable mouse
+vim.opt.mouse = ''
