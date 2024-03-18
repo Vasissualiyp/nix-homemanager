e7c5060bd8b544674ea7fea72adb6819ec083966
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2024-03-18 14:00:43 -0400

    Working noterius plugin keymap

--- modules/editors/neovim/lua/keymaps/noterius.lua
+++ modules/editors/neovim/lua/keymaps/noterius.lua
@@ -6,29 +6,29 @@ vim.keymap.set('n', '<leader>zm', 'ggjVGkkzm<Esc><Esc>gg', { silent = true })
 
 -- Call FindNextNote()
 vim.keymap.set('n', '<leader>nn', function()
-    vim.cmd('call FindNextNote()')
+    vim.cmd('call noterius#FindNextNote()')
 end, { silent = true })
 
 -- Call FindPreviousNote()
 vim.keymap.set('n', '<leader>np', function()
-    vim.cmd('call FindPreviousNote()')
+    vim.cmd('call noterius#FindPreviousNote()')
 end, { silent = true })
 
 -- Call OpenNoteByDate()
 vim.keymap.set('n', '<leader>no', function()
-    vim.cmd('call OpenNoteByDate()')
+    vim.cmd('call noterius#OpenNoteByDate()')
 end, { silent = true })
 
 -- Call DisplayNoteriusQuickhelp()
 vim.keymap.set('n', '<leader>n?', function()
-    vim.cmd('call DisplayNoteriusQuickhelp()')
+    vim.cmd('call noterius#DisplayNoteriusQuickhelp()')
 end, { silent = true })
 
-require('noterius_vim.noterius_telescope').setup({
-  notes_dir = "~/research/notes", -- Custom notes directory
-  keymaps = {
-    ["<leader>nf"] = "search_notes", -- Custom keybind for searching notes
-    ["<leader>ng"] = "grep_notes",   -- Custom keybind for grepping notes
-  }
-})
-
+--require('noterius_vim.noterius_telescope').setup({
+--  notes_dir = "~/research/notes", -- Custom notes directory
+--  keymaps = {
+--    ["<leader>nf"] = "search_notes", -- Custom keybind for searching notes
+--    ["<leader>ng"] = "grep_notes",   -- Custom keybind for grepping notes
+--  }
+--})
+--
