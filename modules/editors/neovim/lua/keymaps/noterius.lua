ce0ef26fd5507a6a8cbc30a8b27b8d7efdbb22e2
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2024-03-19 18:15:42 -0400

    90% working noterius vim plugin

--- modules/editors/neovim/lua/keymaps/noterius.lua
+++ modules/editors/neovim/lua/keymaps/noterius.lua
@@ -1,9 +1,9 @@
 --local notes_dir = vim.fn.expand('~/research/notes')
-vim.g.noterius_notes_dir = '~/research/test'
+vim.g.noterius_notes_dir = '~/research/notes'
 vim.g.noterius_git_url = 'git@github.com:Vasissualiyp/noteriustest.git'
 
 require('noterius-vim.noterius_telescope').setup({
-  notes_dir = notes_dir,
+  notes_dir = vim.g.noterius_notes_dir,
 })
 
 -- These keymaps will only be folding the notes
@@ -11,6 +11,8 @@ vim.keymap.set('n', '<leader>zo', 'ggjVGkkzo<Esc><Esc>gg', { silent = true })
 vim.keymap.set('n', '<leader>zm', 'ggjVGkkzm<Esc><Esc>gg', { silent = true })
 
 -- Noterius note management keybinds
+vim.keymap.set('n', '<leader>N' , ':NoteriusToday<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>nc', ':NoteriusCleanup<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>nn', ':FindNextNote<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>np', ':FindPreviousNote<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>no', ':OpenNoteByDate<CR>', { noremap = true, silent = true })
@@ -20,3 +22,11 @@ vim.keymap.set('n', '<leader>n?', ':DisplayNoteriusQuickhelp<CR>', { noremap = t
 local noterius_telescope = require('noterius-vim.noterius_telescope')
 vim.keymap.set('n','<leader>ng', noterius_telescope.grep_notes)
 vim.keymap.set('n','<leader>nf', noterius_telescope.search_notes)
+
+-- Initialize noterius. MUST BE INCLUDED AFTER the definitions of global variables!
+require('noterius-vim.init').setup({
+  notes_dir = '~/research/notes',
+  author = 'Vasilii Pustovoit',
+  citerius_integration = 1,
+  citerius_src_dir = '~/research/references',
+})
