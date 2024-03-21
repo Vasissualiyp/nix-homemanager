55749dc1b5c35b451fc17bc63ec9ebbc05877b37
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-21 14:18:22 -0400

    Added pulseaudio and backlight

--- modules/WM/waybar/mybar.nix
+++ modules/WM/waybar/mybar.nix
@@ -8,14 +8,14 @@
         layer = "top";
         position = "top";
         spacing = 4; 
-        height = 30;
+        height = 20;
         output = [
           #"eDP-1"
           "HDMI-A-1"
         ];
         modules-left = [ "hyprland/workspaces" "hyprland/mode" "wlr/taskbar" ];
         modules-center = [ "hyprland/window" ];
-        modules-right = [ "network" "pulseaudio" "backlight" "keyboard-state" "battery" "tray" "clock" "date" ];
+        modules-right = [ "network" "pulseaudio" "backlight" "keyboard-state" "battery" "tray" "clock" ];
 
         "hyprland/workspaces" = {
           disable-scroll = true;
@@ -42,18 +42,39 @@
 			];
         };
         "clock" = {
+            # "timezone": "America/New_York",
             tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-            #format-alt = "{:%Y-%m-%d}";
+            format-alt = "{:%Y-%m-%d}";
         };
-        "date" = {
-            tooltip-format = "<big>{:%Y-%m-%d}</big>\n<tt><small>{calendar}</small></tt>";
-            #format-alt = "{:%Y-%m-%d}";
+        "backlight" = {
+            # device = acpi_video1;
+            format = "{icon} {percent}%";
+            format-icons = ["" "" "" "" "" "" "" "" ""];
+        };
+        pulseaudio = {
+            # scroll-step = 1; # %, can be a float
+            format = "{icon} {volume}% {format_source}";
+            format-bluetooth = " {icon} {volume}% {format_source}";
+            format-bluetooth-muted = "󰂲 {icon} {format_source}";
+            format-muted = "󰝟 {format_source}";
+            format-source = " {volume}%";
+            format-source-muted = "";
+            "format-icons" = {
+                headphone = "";
+                hands-free = "";
+                headset = "";
+                phone = "";
+                portable = "";
+                car = "";
+                default = ["" "" ""];
+            };
+            on-click = pavucontrol;
         };
       };
       secondBar = {
         layer = "top";
         position = "top";
-        height = 30;
+        height = 20;
         output = [
           "eDP-1"
           "DP-1"
@@ -62,7 +83,7 @@
         ];
         modules-left = [ "hyprland/workspaces" "hyprland/mode" "wlr/taskbar" ];
         modules-center = [ "hyprland/window" ];
-        modules-right = [ "clock" "temperature" ];
+        modules-right = [ "clock" ];
 
         "hyprland/workspaces" = {
           disable-scroll = true;
