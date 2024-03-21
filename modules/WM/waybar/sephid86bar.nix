c64dd5c19a177ecee5a8ab605c5d64543f82f05f
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-21 10:49:14 -0400

    For the sepheid bar, I altered the modules I need to load, as well as removed unncesessary double quotes for some of the modules

--- modules/WM/waybar/sephid86bar.nix
+++ modules/WM/waybar/sephid86bar.nix
@@ -4,53 +4,56 @@
   programs.waybar = {
     enable = true;
     settings = {
-	    mainBar = {
-        "layer" = "top"; # Waybar at top layer
+      mainBar = {
+        layer = "top"; # Waybar at top layer
         # "position" = "bottom"; # Waybar position (top|bottom|left|right)
-        "height" = 24; # Waybar height (to be removed for auto height)
+        height = 24; # Waybar height (to be removed for auto height)
         # "width" = 1280; # Waybar width
-		output = [ "HDMI-A-1 ];
-        "spacing" = 4; # Gaps between modules (4px)
+		output = [ 
+		  "HDMI-A-1 
+		];
+        spacing = 4; # Gaps between modules (4px)
         # Choose the order of the modules
-        "modules-left" = ["hyprland/workspaces" "hyprland/mode" "hyprland/scratchpad" "custom/media"];
-        "modules-center" = ["hyprland/window"];
-        #"modules-right" = ["mpd", "idle_inhibitor", "pulseaudio", "network", "cpu", "memory", "temperature", "backlight", "keyboard-state", "battery", "battery#bat2", "clock", "tray"];
-        "modules-right" = ["mpd" "idle_inhibitor" "temperature" "cpu" "memory""network" "pulseaudio" "backlight" "keyboard-state" "battery" "battery#bat2" "tray" "clock"];
+        modules-left = ["hyprland/workspaces" "hyprland/mode" "hyprland/scratchpad" "custom/media"];
+        modules-center = ["hyprland/window"];
+        # "modules-right" = ["mpd", "idle_inhibitor", "pulseaudio", "network", "cpu", "memory", "temperature", "backlight", "keyboard-state", "battery", "battery#bat2", "clock", "tray"];
+        #modules-right = ["mpd" "idle_inhibitor" "temperature" "cpu" "memory""network" "pulseaudio" "backlight" "keyboard-state" "battery" "battery#bat2" "tray" "clock"];
+        modules-right = ["network" "pulseaudio" "backlight" "keyboard-state" "battery" "tray" "clock"];
         # Modules configuration
          "hyprland/workspaces" = {
-             "disable-scroll" = true;
-             "all-outputs" = true;
-             "warp-on-scroll" = false;
-             "format" = "{name} = {icon}";
+             disable-scroll = true;
+             all-outputs = true;
+             warp-on-scroll = false;
+             format = {name} = {icon};
              "format-icons" = {
-                 "1" = "";
-                 "2" = "";
-                 "3" = "";
-                 "4" = "";
-                 "5" = "";
-                 "urgent" = "";
-                 "focused" = "";
-                 "default" = "";
+                 1 = ;
+                 2 = ;
+                 3 = ;
+                 4 = ;
+                 5 = ;
+                 urgent = ;
+                 focused = ;
+                 default = ;
              };
          };
         "keyboard-state" = {
-            "numlock" = true;
-            "capslock" = true;
-            "format" = "{name} {icon}";
+            numlock = true;
+            capslock = true;
+            format = {name} {icon};
             "format-icons" = {
-                "locked" = "";
-                "unlocked" = "";
+                locked = ;
+                unlocked = ;
             };
         };
         "hyprland/mode" = {
-            "format" = "<span style=\"italic\">{}</span>";
+            format = <span style=\italic\>{}</span>;
         };
         "hyprland/scratchpad" = {
-            "format" = "{icon} {count}";
-            "show-empty" = false;
-            "format-icons" = ["" ""];
-            "tooltip" = true;
-            "tooltip-format" = "{app} = {title}";
+            format = {icon} {count};
+            show-empty = false;
+            format-icons = [ ];
+            tooltip = true;
+            tooltip-format = {app} = {title};
         };
         "mpd" = {
     #        "format" = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime =%M =%S}/{totalTime =%M =%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
@@ -92,77 +95,77 @@
         };
         "tray" = {
             # "icon-size" = 21;
-            "spacing" = 10;
+            spacing = 10;
         };
         "clock" = {
             # "timezone" = "America/New_York";
-            "tooltip-format" = "<big>{ =%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-            "format-alt" = "{ =%Y-%m-%d}";
+            tooltip-format = <big>{ =%Y %B}</big>\n<tt><small>{calendar}</small></tt>;
+            format-alt = { =%Y-%m-%d};
         };
         "cpu" = {
-            "format" = " {usage}%";
-    #        "tooltip" = false;
+            format =  {usage}%;
+    #        tooltip = false;
         };
         "memory" = {
-            "format" = " {}%";
+            format =  {}%;
         };
         "temperature" = {
-            "thermal-zone" = 2;
-            "hwmon-path" = "/sys/class/hwmon/hwmon1/temp1_input";
-            "critical-threshold" = 80;
-            "format-critical" = "{icon} {temperatureC}°C";
-            "format" = "{icon} {temperatureC}°C";
-            "format-icons" = ["" "" ""];
+            thermal-zone = 2;
+            hwmon-path = /sys/class/hwmon/hwmon1/temp1_input;
+            critical-threshold = 80;
+            format-critical = {icon} {temperatureC}°C;
+            format = {icon} {temperatureC}°C;
+            format-icons = [  ];
         };
         "backlight" = {
-            # "device" = "acpi_video1";
-            "format" = "{icon} {percent}%";
-            "format-icons" = ["" "" "" "" "" "" "" "" ""];
+            # device = acpi_video1;
+            format = {icon} {percent}%;
+            format-icons = [        ];
         };
         "battery" = {
             "states" = {
-                # "good" = 95;
-                "warning" = 30;
-                "critical" = 15;
+                good = 95;
+                warning = 30;
+                critical = 15;
             };
-            "format" = "{icon} {capacity}%";
-            "format-charging" = " {capacity}%";
-            "format-plugged" = " {capacity}%";
-            "format-alt" = "{icon} {time}";
-            # "format-good" = ""; # An empty format will hide the module
-            # "format-full" = "";
-            "format-icons" = ["" "" "" "" ""];
+            format = {icon} {capacity}%;
+            format-charging =  {capacity}%;
+            format-plugged =  {capacity}%;
+            format-alt = {icon} {time};
+            # format-good = ; # An empty format will hide the module
+            # format-full = ;
+            format-icons = [    ];
         };
         "battery#bat2" = {
             "bat" = "BAT2";
         };
         "network" = {
-            # "interface" = "wlp2*"; # (Optional) To force the use of this interface
-            "format-wifi" = "{essid} ({signalStrength}%) ";
-            "format-ethernet" = " {ifname}";
-            "tooltip-format" = " {ifname} via {gwaddr}";
-            "format-linked" = " {ifname} (No IP)";
-            "format-disconnected" = "Disconnected ⚠ {ifname}";
-            "format-alt" = " {ifname} = {ipaddr}/{cidr}";
+            # interface = wlp2*; # (Optional) To force the use of this interface
+            format-wifi = {essid} ({signalStrength}%) ;
+            format-ethernet =  {ifname};
+            tooltip-format =  {ifname} via {gwaddr};
+            format-linked =  {ifname} (No IP);
+            format-disconnected = Disconnected ⚠ {ifname};
+            format-alt =  {ifname} = {ipaddr}/{cidr};
         };
-        "pulseaudio" = {
-            # "scroll-step" = 1; # %, can be a float
-            "format" = "{icon} {volume}% {format_source}";
-            "format-bluetooth" = " {icon} {volume}% {format_source}";
-            "format-bluetooth-muted" = "  {icon} {format_source}";
-            "format-muted" = "  {format_source}";
-            "format-source" = " {volume}%";
-            "format-source-muted" = "";
+        pulseaudio = {
+            # scroll-step = 1; # %, can be a float
+            format = {icon} {volume}% {format_source};
+            format-bluetooth =  {icon} {volume}% {format_source};
+            format-bluetooth-muted =   {icon} {format_source};
+            format-muted =   {format_source};
+            format-source =  {volume}%;
+            format-source-muted = ;
             "format-icons" = {
-    #            "headphone" = "";
-    #            "hands-free" = "";
-    #            "headset" = "";
-    #            "phone" = "";
-    #            "portable" = "";
-    #            "car" = "";
-                "default" = ["" "" ""];
+    #            headphone = ;
+    #            hands-free = ;
+    #            headset = ;
+    #            phone = ;
+    #            portable = ;
+    #            car = ;
+                default = [  ];
             };
-            "on-click" = "pavucontrol";
+            on-click = pavucontrol;
         };
         "custom/media" = {
             "format" = "{icon} {}";
