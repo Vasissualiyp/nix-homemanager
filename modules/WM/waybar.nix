c4d109296863af1dba4dd4db747f97157dedeb33
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-21 10:02:35 -0400

    Updated the waybar to include more modules

--- modules/WM/waybar.nix
+++ modules/WM/waybar.nix
@@ -13,20 +13,12 @@
           "HDMI-A-1"
         ];
         modules-left = [ "hyprland/workspaces" "hyprland/mode" "wlr/taskbar" ];
-        modules-center = [ "hyprland/window" "custom/hello-from-waybar" ];
-        modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
+        modules-center = [ "hyprland/window" ];
+        modules-right = [ "mpd" "battery" "calendar" "clock" "temperature" ];
 
         "sway/workspaces" = {
           disable-scroll = true;
           all-outputs = true;
-        };
-        "custom/hello-from-waybar" = {
-          format = "hello {}";
-          max-length = 40;
-          interval = "once";
-          exec = pkgs.writeShellScript "hello-from-waybar" ''
-            echo "from within waybar"
-          '';
         };
       };
 	};
