7373cfe299b519aea11137e2503c14b820974212
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-21 10:07:01 -0400

    Removing the modules from waybar until it works

--- modules/WM/waybar.nix
+++ modules/WM/waybar.nix
@@ -14,7 +14,7 @@
         ];
         modules-left = [ "hyprland/workspaces" "hyprland/mode" "wlr/taskbar" ];
         modules-center = [ "hyprland/window" ];
-        modules-right = [ "mpd" "battery" "calendar" "clock" "temperature" ];
+        modules-right = [ "mpd" "battery" "temperature" ];
 
         "sway/workspaces" = {
           disable-scroll = true;
