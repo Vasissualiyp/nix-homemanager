11a8ed1ba2bcb5a085731e67e7340363d48231f6
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-21 13:57:41 -0400

    removed workspaces for all outputs, now should only show the current one

--- modules/WM/waybar/mybar.nix
+++ modules/WM/waybar/mybar.nix
@@ -19,7 +19,21 @@
 
         "hyprland/workspaces" = {
           disable-scroll = true;
-          all-outputs = true;
+          all-outputs = false;
+        };
+        "battery" = {
+            "states" = {
+                good = 95;
+                warning = 30;
+                critical = 15;
+            };
+            format = "{icon} {capacity}%";
+            format-charging = " {capacity}%";
+            format-plugged = " {capacity}%";
+            format-alt = "{icon} {time}";
+            # format-good = ; # An empty format will hide the module
+            # format-full = ;
+            format-icons = [         ];
         };
       };
       secondBar = {
@@ -38,21 +52,7 @@
 
         "hyprland/workspaces" = {
           disable-scroll = true;
-          all-outputs = true;
-        };
-        "battery" = {
-            "states" = {
-                good = 95;
-                warning = 30;
-                critical = 15;
-            };
-            format = "{icon} {capacity}%";
-            format-charging = " {capacity}%";
-            format-plugged = " {capacity}%";
-            format-alt = "{icon} {time}";
-            # format-good = ; # An empty format will hide the module
-            # format-full = ;
-            #format-icons = [         ];
+          all-outputs = false;
         };
       };
 	};
