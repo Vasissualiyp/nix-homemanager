53be54669eb180a58a3a91cc79a6ab5d40819e41
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-26 11:01:20 -0400

    Trying to fix qutebroswer coloring

--- modules/web/qutebrowser.nix
+++ modules/web/qutebrowser.nix
@@ -7,7 +7,9 @@
       colors = {
         webppage.preferred_color_scheme = "${config.colorScheme.variant}";
         tabs.bar.bg = "#${config.colorScheme.palette.base00}";
-        keyhint.fg = "#${config.colorScheme.palette.base05}";
+        tabs.bar.fg = "#${config.colorScheme.palette.base05}";
+        hints.bg = "#${config.colorScheme.palette.base00}";
+        hints.fg = "#${config.colorScheme.palette.base05}";
       };
     };
   };
