6537a50dfd7af0ed6eeba89a06eaf2a1729ba602
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-26 11:11:29 -0400

    Removed double-muting

--- modules/WM/hyprland_Vas-Office-Nix.nix
+++ modules/WM/hyprland_Vas-Office-Nix.nix
@@ -245,7 +245,6 @@ in
 		  ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
 		  ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
 		  ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-		  ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
 		  ",XF86KbdBrightnessUp,exec, brightnessctl --device='asus::kbd_backlight' set 5%+"
 		  ",XF86KbdBrightnessDown,exec, brightnessctl --device='asus::kbd_backlight' set 5%-"
           
