17c9c70c5ad62a8ce4fe2c9ccbed238ef26ffb9d
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-26 12:23:45 -0400

    Making webpage dark for dark theme works now

--- modules/web/qutebrowser.nix
+++ modules/web/qutebrowser.nix
@@ -5,8 +5,8 @@
     enable = true;
 	settings = {
       colors = {
-        #webppage.preferred_color_scheme = "${config.colorScheme.variant}";
-        webppage.preferred_color_scheme = "dark";
+        webpage.preferred_color_scheme = "${config.colorScheme.variant}";
+        #webpage.preferred_color_scheme = "dark";
         tabs.bar.bg = "#${config.colorScheme.palette.base00}";
         hints.bg = "#${config.colorScheme.palette.base00}";
         hints.fg = "#${config.colorScheme.palette.base05}";
