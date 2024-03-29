f32e8e2a72b7e5d82cd83ef9c65f8ceee0836587
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-29 10:56:49 -0400

    Working vimwiki link handler

--- modules/editors/neovim/lua/plugins/vimwiki.lua
+++ modules/editors/neovim/lua/plugins/vimwiki.lua
@@ -6,39 +6,39 @@ vim.cmd [[syntax on]]
 -- Custom command
 vim.api.nvim_create_user_command('Diary', 'VimwikiDiaryIndex', {})
 
--- Autocommand group for VimWiki
-local vimwikigroup = vim.api.nvim_create_augroup("vimwikigroup", { clear = true })
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  pattern = "diary.wiki",
-  command = "VimwikiDiaryGenerateLinks",
-  group = vimwikigroup,
-})
-
-function VimwikiLinkHandler(link)
-  local mode = ""
-  if link:match('^zl:') then
-    mode = 'light'
-    link = link:sub(4)
-  elseif link:match('^zd:') then
-    mode = 'dark'
-    link = link:sub(4)
-  else
-    return 0
-  end
-
-  local link_infos = vim.fn['vimwiki#base#resolve_link'](link)
-  if link_infos.filename == '' then
-    print('Vimwiki Error: Unable to resolve link!')
-    return 0
-  else
-    if mode == 'light' then
-      vim.cmd('!zathura -c ~/.config/zathura/zathurarc-light ' .. link_infos.filename)
-    else
-      vim.cmd('!zathura ' .. link_infos.filename)
-    end
-    return 1
-  end
-end
-
--- To expose the Lua function to Vimscript if needed:
-vim.api.nvim_set_var('VimwikiLinkHandler', VimwikiLinkHandler)
+---- Autocommand group for VimWiki
+--local vimwikigroup = vim.api.nvim_create_augroup("vimwikigroup", { clear = true })
+--vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+--  pattern = "diary.wiki",
+--  command = "VimwikiDiaryGenerateLinks",
+--  group = vimwikigroup,
+--})
+--
+--function _G.VimwikiLinkHandler(link)
+--  local mode = ""
+--  if link:match('^zl:') then
+--    mode = 'light'
+--    link = link:sub(4)
+--  elseif link:match('^zd:') then
+--    mode = 'dark'
+--    link = link:sub(4)
+--  else
+--    return 0
+--  end
+--
+--  local link_infos = vim.fn['vimwiki#base#resolve_link'](link)
+--  if link_infos.filename == '' then
+--    print('Vimwiki Error: Unable to resolve link!')
+--    return 0
+--  else
+--    if mode == 'light' then
+--      vim.cmd('!zathura -c ~/.config/zathura/zathurarc-light ' .. link_infos.filename)
+--    else
+--      vim.cmd('!zathura ' .. link_infos.filename)
+--    end
+--    return 1
+--  end
+--end
+--
+---- To expose the Lua function to Vimscript if needed:
+--vim.api.nvim_set_var('VimwikiLinkHandler', VimwikiLinkHandler)
