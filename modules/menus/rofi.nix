1d7431977860f16b43707eb59d3103c281faf8a0
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-31 20:54:46 -0400

    Now everything compiles, but rofi doesn't work properly

--- modules/menus/rofi.nix
+++ modules/menus/rofi.nix
@@ -12,40 +12,22 @@ in {
   programs.rofi = {
     enable = true;
     theme = {
-      "*" = {
-        "background-color" = mkLiteral "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base00}, 1.0)";
-        "foreground-color" = mkLiteral "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base05}, 1.0)";
-        "border-color" = mkLiteral "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base03}, 1.0)";
-        "separator-color" = mkLiteral "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base02}, 1.0)";
-        "width" = mkLiteral "50%";
-        "border" = mkLiteral "2";
+ "*" = {
+        "background-color" = mkLiteral "#000000";
+        "foreground-color" = mkLiteral "rgba (250, 251, 252, 100%)";
+        "border-color" = mkLiteral "#FFFFFF";
+        "width" = 512;
       };
-      "#window" = {
-        background-color = "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base00}, 0.95)";
-        border-color = "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base03}, 1.0)";
-        border = 2;
-        padding = 10;
-        text-color = "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base05}, 1.0)";
+
+      "#inputbar" = {
+        "children" = map mkLiteral [ "prompt" "entry" ];
       };
-      "#listview" = {
-        lines = 5;
-        fixed-height = 1;
-        dynamic = false;
-      };
-      "#element" = {
-        border = 1;
-        padding = 2;
-        border-radius = 5;
-      };
-      "#element selected" = {
-        background-color = "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base02}, 1.0)";
-        text-color = "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base07}, 1.0)";
-      };
-      "#scrollbar" = {
-        handle-color = "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base03}, 1.0)";
-        handle-width = 2;
-        handle-border = 1;
-        handle-border-color = "rgba(${builtins.replaceStrings ["#"] [""] config.colorScheme.palette.base01}, 1.0)";
+
+      "#textbox-prompt-colon" = {
+        "expand" = false;
+        "str" = ":";
+        "margin" = mkLiteral "0px 0.3em 0em 0em";
+        "text-color" = mkLiteral "@foreground-color";
       };
     };
   };
