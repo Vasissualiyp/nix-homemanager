0ba820f8931ef6de800a0e59a5a2982519d061dd
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-06-25 10:04:33 -0400

    Re-riced tmux

--- modules/terminals/tmux.nix
+++ modules/terminals/tmux.nix
@@ -6,48 +6,50 @@
 	terminal = "tmux-256color";
     plugins = with pkgs;
 	[
-	  {
-        plugin = tmuxPlugins.catppuccin;
-        extraConfig = ''
-        set -g @catppuccin_flavour 'mocha'
-		set -g @catppuccin_status_modules_left "session"
-        set -g @catppuccin_status_modules_right "directory date_time"
-        set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
-        set -g @catppuccin_window_default_text "#W"
-        set -g @catppuccin_status_left_separator  ""
-        set -g @catppuccin_status_right_separator ""
-        set -g @catppuccin_window_left_separator ""
-        set -g @catppuccin_window_right_separator ""
-        #set -g @catppuccin_window_middle_separator "█ "
-        set -g @catppuccin_window_middle_separator ""
-        set -g @catppuccin_window_number_position "left"
-        set -g @catppuccin_window_default_fill "number"
-        set -g @catppuccin_status_fill "icon"
-        set -g @catppuccin_status_connect_separator "no"
-        set -g @catppuccin_status_right_separator_inverse "no"
-        set -g @catppuccin_session_text "#S"
-        
-        set -g @catppuccin_directory_icon "#{thm_fg}#[fg=default]"
-        set -g @catppuccin_session_icon "#{thm_fg}#[fg=default]"
-        set -g @catppuccin_window_default_fill "number"
-		''
-        +
-		# Here we interpolate Nix expressions into the string
-        "set -g @catppuccin_pane_color \"#${config.colorScheme.palette.base0B}\"\n" +
-        "set -g @catppuccin_pane_background_color \"#${config.colorScheme.palette.base00}\"\n" +
+	  #{
+      #  plugin = tmuxPlugins.catppuccin;
+      #  extraConfig = ''
+      #  set -g @catppuccin_flavour 'mocha'
+	  #  set -g @catppuccin_status_modules_left "session"
+      #  set -g @catppuccin_status_modules_right "directory date_time"
+      #  set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+      #  set -g @catppuccin_window_default_text "#W"
+      #  set -g @catppuccin_status_left_separator  ""
+      #  set -g @catppuccin_status_right_separator ""
+      #  set -g @catppuccin_window_left_separator ""
+      #  set -g @catppuccin_window_right_separator ""
+      #  #set -g @catppuccin_window_middle_separator "█ "
+      #  set -g @catppuccin_window_middle_separator ""
+      #  set -g @catppuccin_window_number_position "left"
+      #  set -g @catppuccin_window_default_fill "number"
+      #  set -g @catppuccin_status_fill "icon"
+      #  set -g @catppuccin_status_connect_separator "no"
+      #  set -g @catppuccin_status_right_separator_inverse "no"
+      #  set -g @catppuccin_session_text "#S"
+      #  
+      #  #set -g @catppuccin_directory_icon "#${config.colorScheme.palette.base01}#[fg=default]"
+      #  #set -g @catppuccin_session_icon "#${config.colorScheme.palette.base01}#[fg=default]"
+      #  set -g @catppuccin_directory_icon "[fg=default]"
+      #  set -g @catppuccin_session_icon "[fg=default]"
+      #  set -g @catppuccin_window_default_fill "number"
+	  #  ''
+      #  +
+	  #  # Here we interpolate Nix expressions into the string
+      #  "set -g @catppuccin_pane_color \"#${config.colorScheme.palette.base0B}\"\n" +
+      #  "set -g @catppuccin_pane_background_color \"#${config.colorScheme.palette.base00}\"\n" +
 
-        "set -g @catppuccin_window_default_color \"#${config.colorScheme.palette.base05}\"\n" +
-        "set -g @catppuccin_window_default_background \"#${config.colorScheme.palette.base00}\"\n" +
+      #  "set -g @catppuccin_window_default_color \"#${config.colorScheme.palette.base05}\"\n" +
+      #  "set -g @catppuccin_window_default_background \"#${config.colorScheme.palette.base00}\"\n" +
 
-        "set -g @catppuccin_foreground_color \"#${config.colorScheme.palette.base05}\"\n" +
+      #  "set -g @catppuccin_foreground_color \"#${config.colorScheme.palette.base05}\"\n" +
 
-        "set -g @catppuccin_window_current_color \"#${config.colorScheme.palette.base0D}\"\n" +
-        "set -g @catppuccin_window_current_background \"#${config.colorScheme.palette.base0C}\"\n" +
+      #  "set -g @catppuccin_window_current_color \"#${config.colorScheme.palette.base0D}\"\n" +
+      #  "set -g @catppuccin_window_current_background \"#${config.colorScheme.palette.base0C}\"\n" +
 
-        "set -g @catppuccin_directory_color \"#${config.colorScheme.palette.base0E}\"\n" +
+      #  "set -g @catppuccin_directory_color \"#${config.colorScheme.palette.base0E}\"\n" +
 
-        "set -g @catppuccin_session_color \"#${config.colorScheme.palette.base03}\"\n";
-	  }
+      #  "set -g @catppuccin_session_color \"#${config.colorScheme.palette.base03}\"\n";
+	  #}
 	  {
 	    plugin = tmuxPlugins.continuum;
         extraConfig = "set -g @continuum-restore 'on'";
@@ -104,10 +106,7 @@
       bind -n M-H previous-window
       bind -n M-L next-window
       
-      set -g status-position top
       set -g @sessionx-zoxide-mode 'on'
-      
-      
       
       bind-key b send-prefix # Requirement for remote sessions
       
@@ -125,6 +124,45 @@
       
       bind '"' split-window -v -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
+
+      # DESIGN TWEAKS
+      
+      # don't do anything when a 'bell' rings
+      set -g visual-activity off
+      set -g visual-bell off
+      set -g visual-silence off
+      setw -g monitor-activity off
+      set -g bell-action none
+      
+      # clock mode
+      setw -g clock-mode-colour colour1
+      
+      # copy mode
+      setw -g mode-style 'fg=colour1 bg=colour18 bold'
+      
+      # pane borders
+      set -g pane-border-style 'fg=colour1'
+      set -g pane-active-border-style 'fg=colour3'
+      
+      # statusbar
+      set -g status-position top
+      set -g status-justify left
+      set -g status-style 'fg=colour1'
+      set -g status-left '#[fg=colour14,bg=colour16] #S '
+      set -g status-right '%Y-%m-%d %H:%M '
+      set -g status-right-length 50
+      set -g status-left-length 10
+      
+      setw -g window-status-current-style 'fg=colour1 bg=colour14 bold'
+      setw -g window-status-current-format ' #I #W #F '
+      
+      setw -g window-status-style 'fg=colour1 dim'
+      setw -g window-status-format ' #I #[fg=colour7]#W #[fg=colour1]#F '
+      
+      setw -g window-status-bell-style 'fg=colour2 bg=colour1 bold'
+      
+      # messages
+      set -g message-style 'fg=colour2 bg=colour0 bold'
 	'';
   };
 }
