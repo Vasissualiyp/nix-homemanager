05840b4f55b0664d9a1a7c4226b903f45e897c76
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-06-26 23:15:28 -0400

    Changed the directory of the snippets, so that I don't have to ln -s every time I change a machine

--- modules/editors/neovim/lua/plugins/luasnip.lua
+++ modules/editors/neovim/lua/plugins/luasnip.lua
@@ -1,5 +1,5 @@
 -- Load snippets from ~/.config/nvim/LuaSnip/
-require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+require("luasnip.loaders.from_lua").load({paths = "~/.dotfiles/nvim/LuaSnip/"})
 --require("luasnip").config.set_config({enable_autosnippets = true})
 require("luasnip").config.set_config({ -- Setting LuaSnip config
   -- Use <Tab> (or some other key if you prefer) to trigger visual selection
