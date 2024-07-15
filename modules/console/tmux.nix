70f2c8aff267e3245f2fd214994e6bd9c852f085
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-07-15 17:37:46 -0400

    kitty now is defined in nix"

--- modules/console/tmux.nix
+++ modules/console/tmux.nix
@@ -12,18 +12,17 @@
       }
 	  tmuxPlugins.sensible
 	  tmuxPlugins.yank
-	  {
-	    plugin = tmuxPlugins.resurrect;
-		extraConfig = ''
-	    set -g @continuum-restore 'on'
-	    set -g @resurrect-strategy-vim 'session'
-        set -g @resurrect-strategy-nvim 'session'
-        set -g @resurrect-capture-pane-contents 'on'
-        resurrect_dir="$HOME/.tmux/resurrect"
-        set -g @resurrect-dir $resurrect_dir
-        set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/home/$USER/.nix-profile/bin/||g" $target | sponge $target'
-		'';
-	  }
+      tmuxPlugins.resurrect
+	  #{
+	  #  extraConfig = ''
+	  #  set -g @resurrect-strategy-vim 'session'
+      #  set -g @resurrect-strategy-nvim 'session'
+      #  set -g @resurrect-capture-pane-contents 'on'
+      #  resurrect_dir="$HOME/.tmux/resurrect"
+      #  set -g @resurrect-dir $resurrect_dir
+      #  set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/home/$USER/.nix-profile/bin/||g" $target | sponge $target'
+	  #  '';
+	  #}
 	  tmuxPlugins.continuum
 	  tmuxPlugins.tmux-fzf
 	  tmuxPlugins.vim-tmux-navigator
