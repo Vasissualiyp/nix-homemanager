bb0bef3d95a20d97872f205af3cb2765b588c40a
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-08-26 18:25:48 -0400

    Added new 5-monitor layout for when Dylan left

--- hosts/Vas-Office-Nix/5mon.nix
+++ hosts/Vas-Office-Nix/5mon.nix
@@ -14,8 +14,8 @@ in
 	  width = 2560;
 	  height = 1600;
 	  refreshRate = 60;
-	  x = 0; 
-	  y = 200;
+	  x = 4720; 
+	  y = 1200;
 	  scale = 2;
 	  enabled = true;
 	}
@@ -23,39 +23,37 @@ in
 	  name = "DP-5";
 	  width = 1920; 
 	  height = 1200;
-	  x = 1280;
+	  x = 4080;
 	  y = 0;
 	  scale = 1;
-	  hypr_transform = "transform,1";
 	  enabled = true;
     }
     {
 	  name = "HDMI-A-1";
 	  width = 3840; 
 	  height = 2160;
-	  x = 2480;
+	  x = 0;
 	  y = 0;
 	  refreshRate = 60;
-	  hypr_transform = "transform,1";
 	  scale = 2;
+	  hypr_transform = "transform,1";
 	  enabled = true;
     }
     {
 	  name = "DP-1";
-	  width = 1920; 
-	  height = 1200;
-	  x = 3560;
-	  y = 0;
+	  width = 2560; 
+	  height = 1600;
+	  x = 2160;
+	  y = 1200;
 	  scale = 1;
-	  hypr_transform = "transform,1";
 	  enabled = true;
     }
     {
 	  name = "DP-4";
 	  width = 1920;
 	  height = 1080;
-	  x = 4760;
-	  y = 420;
+	  x = 2160;
+	  y = 0;
 	  scale = 1;
 	  enabled = true;
 	}
