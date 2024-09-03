8cf30845bc49f128ca036f98e0a6054049db995c
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-09-03 16:55:23 -0400

    Updated hyprland animations

--- modules/WM/hyprland.nix
+++ modules/WM/hyprland.nix
@@ -182,18 +182,23 @@ in
         };
         
         animations = {
-            "enabled" = "yes";
+          "enabled" = "yes";
         
-            # Animations
+          # Animations
         
-            "bezier" = "myBezier, 0.05, 0.9, 0.1, 1.05";
+          "bezier" = [ 
+		    "myBezier, 0.05, 0.9, 0.1, 1.05"
+            "easeInOutBack, 0.68, -0.6, 0.32, 1.6"
+            "easeOutCubic, 0.33, 1, 0.68, 1"
+		  ];
+
   	      animation = [
-             "windows, 1, 7, myBezier"
+             "windows, 1, 5, easeOutCubic"
              "windowsOut, 1, 7, default, popin 80%"
              "border, 1, 10, default"
              "borderangle, 1, 8, default"
              "fade, 1, 7, default"
-             "workspaces, 1, 6, default"
+             "workspaces, 1, 5, easeInOutBack, slidefadevert 80%"
   	      ];
         };
         
