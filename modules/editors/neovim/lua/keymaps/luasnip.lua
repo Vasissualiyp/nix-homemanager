def322bce05f00e591d55cee22dca4eba7dd8e23
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-10-18 16:51:16 -0400

    Made luasnip keybinds working

--- modules/editors/neovim/lua/keymaps/luasnip.lua
+++ modules/editors/neovim/lua/keymaps/luasnip.lua
@@ -18,4 +18,16 @@ smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '
 --vim.api.nvim_set_keymap('i', '<C-k>', '<Plug>luasnip-prev-choice', {})
 --im.api.nvim_set_keymap('s', '<C-k>', '<Plug>luasnip-prev-choice', {})
 
+-- Here are actual luasnip keybinds
 
+local ls = require("luasnip")
+
+vim.keymap.set({"i"}, "<C-J>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-H>", function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, {silent = true})
